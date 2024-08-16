7073b88092731382eeeb0f00d3d12226518f200a
1102158249@qq.com <liu159357@>
2024-08-16 11:44:06 +0800

    修改启动类名字

---
+++ sensitive-util/src/main/java/com/hongda/SensitiveApplication.java
@@ -0,0 +1,10 @@
+package com.hongda;
+
+import org.springframework.boot.SpringApplication;
+
+public class SensitiveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SensitiveApplication.class, args);
+  }
+}

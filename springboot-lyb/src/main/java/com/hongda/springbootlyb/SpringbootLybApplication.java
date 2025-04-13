eabc38059f996926162d80d555e1b526a5a09950
1102158249@qq.com <Liu159357@$>
2025-04-13 23:45:04 +0800

    搭建简单服务

---
+++ springboot-lyb/src/main/java/com/hongda/springbootlyb/SpringbootLybApplication.java
@@ -0,0 +1,13 @@
+package com.hongda.springbootlyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLybApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLybApplication.class, args);
+  }
+
+}

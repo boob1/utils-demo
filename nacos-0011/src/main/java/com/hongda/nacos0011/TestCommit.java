33eb0fee314cba5980d1a1f3ccfddf40ec27e2b4
liuyibo <Liu159357@$>
2025-06-12 09:02:19 +0800

    提交

--- nacos-0011/src/main/java/com/hongda/nacos0011/TestCommit.java
+++ nacos-0011/src/main/java/com/hongda/nacos0011/TestCommit.java
@@ -9,6 +9,9 @@ public class TestCommit {
     public static void main(String[] args) {
         System.out.println("A测试提交");
       System.out.println("B测试提交");
+      System.out.println("C测试提交");
+      System.out.println("D测试提交");
+
     }
 
 }

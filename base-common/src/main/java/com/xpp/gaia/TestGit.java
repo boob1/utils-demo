4f82d6702483aa79691c1edc430d226c5cbfacca
liuyibo <Liu159357@$>
2025-06-13 14:50:06 +0800

    第一次提交

---
+++ base-common/src/main/java/com/xpp/gaia/TestGit.java
@@ -0,0 +1,14 @@
+package com.xpp.gaia;
+
+/**
+ * @Description
+ * @Author lyb
+ * @Date 2025/6/13 14:49
+ */
+public class TestGit {
+
+  public static void main(String[] args) {
+    System.out.println("第一次提交");
+  }
+
+}

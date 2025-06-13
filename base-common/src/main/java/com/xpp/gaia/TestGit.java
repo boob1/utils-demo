e4d56a85254b25b6640a09ad56a6e25e243dd2f0
liuyibo <Liu159357@$>
2025-06-13 15:54:05 +0800

    Revert "第2次提交"
    This reverts commit d906fc1a

--- base-common/src/main/java/com/xpp/gaia/TestGit.java
+++ base-common/src/main/java/com/xpp/gaia/TestGit.java
@@ -9,12 +9,6 @@ public class TestGit {
 
   public static void main(String[] args) {
     System.out.println("第一次提交");
-
-    System.out.println("第2次提交");
-
-    System.out.println("第3次提交");
-
-    System.out.println("第4次提交");
   }
 
 }

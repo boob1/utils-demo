7b49a1b18ddced4213eaadc6a2f785d4282fbe7f
1102158249@qq.com <Liu159357@$>
2025-07-12 22:42:28 +0800

    redis

---
+++ redis-utils-15/src/main/java/com/hongda/redisutils15/RedisUtils15Application.java
@@ -0,0 +1,13 @@
+package com.hongda.redisutils15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisUtils15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisUtils15Application.class, args);
+    }
+
+}
---
+++ redis-utils-15/src/main/java/com/hongda/redisutils15/controller/RedisController.java
@@ -0,0 +1,30 @@
+package com.hongda.redisutils15.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: RedisController
+ * @description:
+ * @author: sh.Liu
+ * @date: 2022-03-08 14:28
+ */
+@RestController
+@RequestMapping("redis")
+public class RedisController {
+
+    private final RedisTemplate redisTemplate;
+
+    public RedisController(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("save")
+    public String save(String key, String value){
+        redisTemplate.opsForValue().set(key, value);
+        return "保存成功！";
+    }
+
+}
